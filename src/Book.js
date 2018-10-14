46e9708b12761a919ca54e15d8eb762276ac7a42
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-10-14 12:26:32 -0700

    book component

--- src/Book.js
+++ src/Book.js
@@ -1,29 +1,35 @@
 import React, { Component} from 'react' ;
-
 class Book extends Component {
+  // renders the info of each book to the bookmark in the main page
   render() {
+    let  dislayedThumbnail = this.props.book.imageLinks ?
+    this.props.book.imageLinks.thumbnail :  '';
+
     return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book
-          .imageLinks.thumbnail}")` }}></div>
-          <div className="book-shelf-changer">
-            <select>
+      <section className="book">
+        <section className="book-top">
+          <section className="book-cover" 
+          style={{ width: 128, height: 193, backgroundImage: `url("${dislayedThumbnail}")` }}>
+          </section>
+          <section className="book-shelf-changer">
+            <select
+            onChange= {(event) => this.props.moveBook(
+            this.props.book, event.target.value
+            )}
+            value={this.props.currentShelf}
+            >
               <option value="move" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>
               <option value="none">None</option>
             </select>
-          </div>
-        </div>
-        <div className="book-title">{this.props.book.title} </div>
-        <div className="book-authors">{this.props.book.authors} </div>
-      </div>
-     
+          </section>
+        </section>
+        <section className="book-title">{this.props.book.title} </section>
+        <section className="book-authors">{this.props.book.authors} </section>
+      </section>     
     );
   }
-
 }
-
 export default Book;
