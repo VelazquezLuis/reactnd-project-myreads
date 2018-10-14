7489bb6d1e9a932eb9dd00cebd5c940964d548d3
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-10-14 12:26:21 -0700

    router installed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
 import React from 'react' ;
-
+import { Route } from 'react-router-dom';
 import SearchPage from './SearchPage';
 import MainPage from './MainPage';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
+//makes state book an array
 class BooksApp extends React.Component {
   state = {
     books: []
   }
-
+  //sets the state to book
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books: books })
     })
   }
-  
+
+  moveBook = (book, shelf) => {
+    BooksAPI.update(book, shelf); // this updates the books on change.
+    BooksAPI.getAll().then( (books) => {
+      this.setState({books : books})
+    })    
+  }
+  // connects the pages together
   render() {
-    
     return (
-      <div className="app">
-        <MainPage books={this.state.books} />
-      </div>
+      <section className="app">
+        <Route exact path="/" render={() => (
+          <MainPage
+            books={this.state.books}
+            moveBook={this.moveBook}
+          />
+        )} />
+        
+        <Route path="/search" render={() => (
+          <SearchPage
+          moveBook={this.moveBook}
+          books={this.state.books}
+          />
+        )} />
+      </section>
     )
   }
 }
