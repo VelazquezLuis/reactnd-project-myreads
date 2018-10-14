bcab906e3f9ab1a88bdf35a7874829e8d6ee442f
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-10-14 12:27:37 -0700

    router instered

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
+//creates a router to conect all pages
+ReactDOM.render(
+  <BrowserRouter><App/></BrowserRouter>,
+  document.getElementById('root'))
